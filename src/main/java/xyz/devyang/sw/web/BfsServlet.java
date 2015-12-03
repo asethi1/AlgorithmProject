3d21d7db7a21758772a749a23899c64689432604
yangyu <yangyu9415@gmail.com>
2015-12-03 17:35:23 -0500

    final changes

--- src/main/java/xyz/devyang/sw/web/BfsServlet.java
+++ src/main/java/xyz/devyang/sw/web/BfsServlet.java
@@ -1,6 +1,8 @@
 package xyz.devyang.sw.web;
 
+import xyz.devyang.sw.core.Graph;
 import xyz.devyang.sw.core.Node;
+import xyz.devyang.sw.core.RuntimeCache;
 import xyz.devyang.sw.core.SearchEngine;
 import xyz.devyang.sw.storage.Serialization;
 
@@ -11,6 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by YangYu on 12/2/15.
@@ -26,7 +31,9 @@ public class BFSServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-//        RuntimeCache.GRAPH = (Graph) Serialization.readObject("63392-2015-11-30-17-34-43.dat");
+        System.out.println("Start reading...");
+        RuntimeCache.GRAPH = (Graph) Serialization.readObject("/Users/yangyu/Documents/15Fall/Algo/project/SmallWorld/scc/63392-2015-11-30-17-34-43.dat");
+        System.out.println("Finish reading...");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -36,24 +43,32 @@ public class BFSServlet extends HttpServlet {
         start = request.getParameter("startVertex");
         PrintWriter writer = response.getWriter();
 
-        if (times==null||start==null||!times.matches("^\\d+$")||!start.matches("^\\d+$")) {
-            request.getRequestDispatcher("second.jsp?path=wrong_input").forward(request, response);
+        if (start==null||!start.matches("^\\d+$")) {
+            request.getRequestDispatcher("index.jsp").forward(request, response);
         } else {
-//            int t = Integer.parseInt(times);
-//            int s = Integer.parseInt(start);
-//            Node node = RuntimeCache.GRAPH.getNodes().get(s);
-//            Map<String, Object> map = null;
-//            for (int i=0;i<t;i++) {
-//    //            map.put("start", node);
-//    //            map.put("end", last);
-//    //            map.put("path", path);
-//                map = searchEngine.oneTimeBFS(node);
-//                node = (Node) map.get("end");
-//            }
-//            List path = (List) map.get("path");
-//            System.out.println(Arrays.toString(path.toArray()));
-            request.setAttribute("path",15);
-            request.getRequestDispatcher("second.jsp").forward(request, response);
+            if (times == null || !times.matches("^\\d+$")) {
+                times = "2";
+            }
+            int t = Integer.parseInt(times);
+            int s = Integer.parseInt(start);
+            Node node = RuntimeCache.GRAPH.getNodes().get(s);
+            if (node == null) {
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+                return;
+            }
+            Map<String, Object> map = null;
+            for (int i=0;i<t;i++) {
+    //            map.put("start", node);
+    //            map.put("end", last);
+    //            map.put("path", path);
+                map = searchEngine.oneTimeBFS(node);
+                node = (Node) map.get("end");
+            }
+            List path = (List) map.get("path");
+            System.out.println(Arrays.toString(path.toArray()));
+            request.setAttribute("path",path.size()-1);
+            request.setAttribute("list", Arrays.toString(path.toArray()));
+            request.getRequestDispatcher("index.jsp").forward(request, response);
 //            writer.close();
         }
     }
---
+++ web/test.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+    <form action="/search" method="post">
+      Times: <input type="text" name="times"><br/>
+      Start from: <input type="text" name="start"><br/>
+      <input type="submit">
+    </form>
+  </body>
+</html>
